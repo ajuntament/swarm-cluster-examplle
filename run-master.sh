e0ee8d76fc08aad8387c2d595a788c592740507a
ajuntament <fmartinez@tarragona.cat>
2020-10-28 16:46:58 +0000

    Modifico run master

--- run-master.sh
+++ run-master.sh
@@ -1,21 +1,26 @@
 apt install -y git
 curl https://get.docker.com | sudo bash
 
-mkdir -p /srv/docker
+## Miscelanea
+echo 'colorscheme desert' >> /etc/vim/vimrc
+
+mkdir -p /opt/docker
 mkdir -p /srv/nfs
+
 #	Configurar NFS
-echo 'maquina-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+echo 'maquina-01:/srv/nfs /opt/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-kernel-server
-echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+systemctl enable nfs-kernel-server
 systemctl start nfs-kernel-server
+echo '/srv/nfs maquina-01(rw,no_root_squash,no_subtree_check) maquina-02(rw,no_root_squash,no_subtree_check) maquina-03(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+systemctl restart nfs-kernel-server
+#####echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+exportfs -r
 mount -a
 
 docker swarm init 
-docker swarm join-token manager|grep join  > /srv/docker/join.sh
-chmod +x !$
-
-cd /srv/docker
-git clone https://github.com/kpeiruza/swarm-cluster-example
+docker swarm join-token manager|grep join  > /opt/docker/join.sh
+chmod +x /opt/docker/join.sh
 
 docker network create proxy -d overlay
 docker network create portainer_agent -d overlay
