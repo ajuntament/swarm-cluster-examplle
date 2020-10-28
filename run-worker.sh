b88c61061e4af0d75dd8a5c7ef1179b27a5e90a7
ajuntament <fmartinez@tarragona.cat>
2020-10-28 16:50:44 +0000

    Modifico run worker

--- run-worker.sh
+++ run-worker.sh
@@ -1,13 +1,17 @@
 apt install -y git
 curl https://get.docker.com | sudo bash
 
-mkdir -p /srv/docker
+## Miscelanea
+echo 'colorscheme desert' >> /etc/vim/vimrc
+
+mkdir -p /opt/docker
+
 #	Configurar NFS
-echo 'maquina-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+echo 'maquina-01:/srv/nfs /opt/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-common
-echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+
 mount -a
 
-/srv/docker/join.sh
+/opt/docker/join.sh
 
 
