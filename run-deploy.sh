a117fd82be76cea965a66fb381dbcc0715272e4d
ajuntament <fmartinez@tarragona.cat>
2020-10-28 16:53:58 +0000

    Modifico run deploy

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
-IP1=$(getent hosts instance-1 | awk '{print $1}')
-IP2=$(getent hosts instance-2 | awk '{print $1}')
-IP3=$(getent hosts instance-3 | awk '{print $1}')
-export IP1 IP2 IP3
-export REMOTE_MOUNT="/srv/docker/data"
+function create_dir {
+    if [ ! -d $1 ]; then
+        mkdir -p $1
+        chmod -R 1777 $1
+    fi 
+}
 
-cd /srv/docker/swarm-cluster-example
-docker stack deploy -c traefik/stack.yaml traefik
-docker stack deploy -c portainer/stack.yaml portainer
-docker stack deploy -c consul-cluster/stack.yaml consul-cluster
-docker stack deploy -c comptador/stack.yaml comptador
+function deploy {
+    docker stack deploy -c ${BASEDIR}/$1/docker-compose.yml $1
+}
+
+IP1=$(curl ifconfig.me)
+IP2=$(getent hosts maquina-02 | awk '{print $1}')
+IP3=$(getent hosts maquina-03 | awk '{print $1}')
+export IP1 IP2 IP3
+
+BASEDIR=$(dirname $0)
+
+if [ $# -eq 1 ]; then
+    deploy $1
+    exit 0
+fi
+
+deploy traefik
+
+create_dir /opt/docker/portainer/volumes/portainer
+deploy portainer
+
+#deploy consul-cluster
+#deploy comptador
+
+create_dir /opt/docker/wordpress/volumes/database
+deploy wordpress
+
+create_dir /opt/docker/elastic/volumes/elasticsearch
+deploy elastic
+
+create_dir /opt/docker/monitor/volumes/grafana
+create_dir /opt/docker/monitor/volumes/influxdb
+deploy monitor
